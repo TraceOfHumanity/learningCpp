f9f91b859e1b6ffbb5a280503a235c0bae7ba135
Alexander_Ovchar <99219332+TraceOfHumanity@users.noreply.github.com>
2023-07-18 19:29:34 +0300

    Create circle.cpp

---
+++ EnteringInformation/circle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Enter the radius of the circle: ";
+	double radius;
+	std::cin >> radius;
+
+	double diameter = radius * 2;
+	const double PI = 3.14159;
+	double area = PI * radius * radius;
+	double length = 2 * PI * radius;
+
+	std::cout << "------------circle information ------------\n" << std::endl;
+	std::cout << "Radius: " << radius << std::endl;
+	std::cout << "Diameter: " << diameter << std::endl;
+	std::cout << "Area: " << area << std::endl;
+	std::cout << "Length: " << length << std::endl;
+
+	return 0;
+
+}
