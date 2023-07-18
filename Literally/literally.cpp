8c1d29adeb57716fde5cb45199f5a7655c01d89f
Alexander_Ovchar <99219332+TraceOfHumanity@users.noreply.github.com>
2023-07-18 20:58:20 +0300

    Create literally.cpp

---
+++ Literally/literally.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+	short a = 10;
+	int b =10;
+	long c = 11L;
+
+	unsigned short a = 10;
+	unsigned int b = 10U;
+	unsigned long c = 11UL;
+	unsigned long long c = 11ULL;
+
+	float a = 1.0F;
+	double b = 1.0;
+	long double c = 1.0L;
+
+	char a = 'a';
+	char16_t b = u'a';
+	char32_t c = U'a';
+	wchar_t d = L'a';
+
+	bool a = true;
+
+	return 0;
+
+}
