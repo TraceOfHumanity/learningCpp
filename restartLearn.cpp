29e346dc3ddeb0faa26cc0caf3d844da7dd7e569
Alexander_Ovchar <azalexxx02@gmail.com>
2023-11-17 20:09:26 +0200

    literals

--- restartLearn.cpp
+++ restartLearn.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
-
+// literals
 int main()
 {
-  int a, b;
-  int sum;
-  std::cout << "Enter two numbers: ";
-  std::cin >> a;
-  std::cin >> b;
-  sum = a + b;
-  std::cout << "Sum is: " << sum << std::endl;
+  short a = 10;
+  int b = 140;
+  long c = 1000L;
+  long long d = 10000LL;
+
+  unsigned short aa = 10;
+  unsigned int bb = 10U;
+  unsigned long cc = 1000UL;
+  unsigned long long dd = 10000ULL;
+
+  float e = 3.14F;
+  double f = 3.14;
+  long double g = 3.14L;
+
+  char h = 'a';
+  wchar_t i = L'a';
+  char16_t j = u'a';
+  char32_t k = U'a';
+
+  bool j = true;
+  bool k = false;
+
+
+
   return 0;
 }
