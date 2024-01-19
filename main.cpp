e6996cec045638627c78351d0342791d2d239100
Alexander_Ovchar <azalexxx02@gmail.com>
2024-01-19 18:48:03 +0200

    reload functions

--- main.cpp
+++ main.cpp
@@ -2,34 +2,64 @@
 #include <cstdlib>
 #include <ctime>
 
-void InitializeArray(char array[], int size);
-void ShowArray(char array[], int size);
+void Show(const int array[], int size);
+void Show(const char array[], int size);
 
 int main()
 {
-    // srand((unsigned)time(nullptr));
-    const int size = 26;
-    char alphabet[size];
+    srand(time(NULL));
+    const int size = 10;
+    const char alphabet = 26;
+    int array[size];
+    char array2[alphabet] = {
+        'a',
+        'b',
+        'c',
+        'd',
+        'e',
+        'f',
+        'g',
+        'h',
+        'r',
+        'j',
+        'k',
+        'l',
+        'm',
+        'n',
+        'o',
+        'p',
+        'q',
+        's',
+        't',
+        'u',
+        'v',
+        'w',
+        'x',
+        'y',
+        'z'};
 
-    InitializeArray(alphabet, size);
-    ShowArray(alphabet, size);
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand() % 10;
+        array2[i] = 'a';
+    }
+
+    Show(array, size);
+    Show(array2, alphabet);
 
     return 0;
 }
 
-void InitializeArray(char array[], int size)
+void Show(const int array[], int size)
 {
-    for (int i = 0; i < size; ++i)
-    {
-        array[i] = 'A' + i;
-    }
+    for (int i = 0; i < size; i++)
+        std::cout << array[i] << " ";
+    std::cout << std::endl;
 }
 
-void ShowArray(char array[], int size)
+void Show(const char array[], int size)
 {
-    for (int i = 0; i < size; ++i)
-    {
+    for (int i = 0; i < size; i++)
         std::cout << array[i] << " ";
-    }
     std::cout << std::endl;
 }
