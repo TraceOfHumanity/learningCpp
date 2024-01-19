8e4b44144f55558e9912a6ad3133e87a4d8d5511
Alexander_Ovchar <azalexxx02@gmail.com>
2024-01-19 17:12:36 +0200

    to cube function

--- functions.cpp
+++ functions.cpp
@@ -5,12 +5,18 @@ int Add(int a, int b)
   return a + b;
 }
 
+double toCube(double a)
+{
+  return a * a * a;
+}
+
 int main()
 {
   int a = 5;
   int b = 10;
 
   std::cout << Add(a, b) << std::endl;
+  std::cout << "cube: " << toCube(a) << std::endl;
 
   return 0;
 }
