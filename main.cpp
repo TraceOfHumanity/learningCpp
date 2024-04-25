b55332ab891f9704c8ae94d406729c122197a84c
Alexander_Ovchar <azalexxx02@gmail.com>
2024-04-25 14:42:29 +0300

    std::cout

--- main.cpp
+++ main.cpp
@@ -2,20 +2,6 @@
 
 int main()
 {
-    int *ptr = new int{5};
-    const int *const ptr2 = new int{10};
-    int **ptr3 = new int *{nullptr};
-
-    std::cout << *ptr << std::endl;
-    std::cout << *ptr2 << std::endl;
-    std::cout << *ptr3 << std::endl;
-
-    delete ptr;
-    delete ptr2;
-    delete ptr3;
-
-    std::cout << *ptr << std::endl;
-    std::cout << *ptr2 << std::endl;
-    std::cout << *ptr3 << std::endl;
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
