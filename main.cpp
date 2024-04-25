e4e8c233a5e070f5b43b7d047a074c25feb4a7c3
Alexander_Ovchar <azalexxx02@gmail.com>
2024-04-25 15:43:21 +0300

    std::cout

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 
 int main()
 {
-    std::cout << "Enter a number: ";
-    int x;
-    std::cin >> x;
-    std::cout << "You entered " << x << '\n';
-
+    int num1, num2;
+    int sum;
+    std::cout << "Enter two numbers: ";
+    std::cin >> num1 >> num2;
+    sum = num1 + num2;
+    std::cout << "The sum of " << num1 << " and " << num2 << " is " << sum << std::endl;
     return 0;
 }
