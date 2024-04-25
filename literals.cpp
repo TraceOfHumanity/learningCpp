ba3d47d67ca444bb7187c2c5c96d834c29908789
Alexander_Ovchar <azalexxx02@gmail.com>
2024-04-25 16:07:32 +0300

    literals

---
+++ literals.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+int main()
+{
+  short a = 10;
+  int b = 40;
+  long c = 11L;
+  long long d = 10LL;
+
+  unsigned short a = 10;
+  unsigned int b = 10U;
+  unsigned long c = 10UL;
+  unsigned long long d = 10ULL;
+
+  float a = 10.0F;
+  double b;
+  long double c = 10.0L;
+
+  char a = 'a';
+  wchar_t b = L'a';
+  char16_t c = u'a';
+  char32_t d = U'a';
+
+  bool a;
+
+  return 0;
+}
