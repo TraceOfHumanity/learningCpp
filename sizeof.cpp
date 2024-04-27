140be0f0a2916f65b2802d59ddffe6e40e8e3288
Alexander_Ovchar <azalexxx02@gmail.com>
2024-04-27 14:31:22 +0300

    sizeof

---
+++ sizeof.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Size of char: " << sizeof(char) << " byte" << std::endl;
+  std::cout << "Size of wchar_t: " << sizeof(wchar_t) << " byte" << std::endl;
+  std::cout << "Size of char16_t: " << sizeof(char16_t) << " byte" << std::endl;
+  std::cout << "Size of char32_t: " << sizeof(char32_t) << " byte" << std::endl;
+
+  std::cout << std::endl;
+
+  std::cout << "Size of short: " << sizeof(short) << " bytes" << std::endl;
+  std::cout << "Size of unsigned short: " << sizeof(unsigned short) << " bytes" << std::endl;
+  std::cout << "Size of int: " << sizeof(int) << " bytes" << std::endl;
+  std::cout << "Size of unsigned int: " << sizeof(unsigned int) << " bytes" << std::endl;
+  std::cout << "Size of long: " << sizeof(long) << " bytes" << std::endl;
+  std::cout << "Size of unsigned long: " << sizeof(unsigned long) << " bytes" << std::endl;
+  std::cout << "Size of long long: " << sizeof(long long) << " bytes" << std::endl;
+  std::cout << "Size of unsigned long long: " << sizeof(unsigned long long) << " bytes" << std::endl;
+
+  std::cout << std::endl;
+
+  std::cout << "Size of float: " << sizeof(float) << " bytes" << std::endl;
+  std::cout << "Size of double: " << sizeof(double) << " bytes" << std::endl;
+  std::cout << "Size of long double: " << sizeof(long double) << " bytes" << std::endl;
+  
+  return 0;
+}
