da076c8de7a87ffa51f6aa3677ddd3a1ebb7f26c
Alexander_Ovchar <azalexxx02@gmail.com>
2024-04-27 14:12:06 +0300

    castingDataTypes

--- literals.cpp
+++ literals.cpp
@@ -6,21 +6,21 @@ int main()
   long c = 11L;
   long long d = 10LL;
 
-  unsigned short a = 10;
-  unsigned int b = 10U;
-  unsigned long c = 10UL;
-  unsigned long long d = 10ULL;
-
-  float a = 10.0F;
-  double b;
-  long double c = 10.0L;
-
-  char a = 'a';
-  wchar_t b = L'a';
-  char16_t c = u'a';
-  char32_t d = U'a';
-
-  bool a;
+//  unsigned short a = 10;
+//  unsigned int b = 10U;
+//  unsigned long c = 10UL;
+//  unsigned long long d = 10ULL;
+//
+//  float a = 10.0F;
+//  double b;
+//  long double c = 10.0L;
+//
+//  char a = 'a';
+//  wchar_t b = L'a';
+//  char16_t c = u'a';
+//  char32_t d = U'a';
+//
+//  bool a;
 
   return 0;
 }
