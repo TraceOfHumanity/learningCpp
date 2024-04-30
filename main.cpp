535a16406e07d8088f41d72c6d924b12de3d6148
Alexander_Ovchar <azalexxx02@gmail.com>
2024-05-01 01:20:53 +0300

    init pointers

--- main.cpp
+++ main.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 
-double ToCube (double number);
+double ToCube(double number);
 
 int main()
 {
-  double number;
-  std::cout << "Enter a number: ";
-  std::cin >> number;
-  std::cout << "The cube of " << number << " is " << ToCube(number) << std::endl;
-  
+  int a = 4;
+
+  int *p = &a;
+
+  std::cout << "a = " << a << std::endl;
+  std::cout << "&a = " << &a << std::endl;
+  std::cout << "p = " << p << std::endl;
+  std::cout << "*p = " << *p << std::endl;
+
+  *p = 5;
+
+  std::cout << "a = " << a << std::endl;
+  std::cout << "&a = " << &a << std::endl;
+  std::cout << "p = " << p << std::endl;
+  std::cout << "*p = " << *p << std::endl;
   return 0;
 }
