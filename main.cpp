564ebf1d5927815ced6443e086296cd8c9b7ff27
Alexander_Ovchar <azalexxx02@gmail.com>
2024-05-01 03:29:13 +0300

    universal functions with pointers

--- main.cpp
+++ main.cpp
@@ -1,14 +1,45 @@
 #include <iostream>
 
-void Change(int *a){
-  *a = 10;
-}
+void InitializeArray(int array[], int size);
+void ShowArray(int array[], int size, void (*showElement)(int element));
+void ShowElement(int element);
+void ShowElementInBrackets(int element);
+void ShowElementInSquareBrackets(int element);
 
 int main()
 {
-  int a = 5;
-  Change(&a);
-  std::cout << a << std::endl;
+  const int SIZE = 10;
+  int array[SIZE];
+
+  InitializeArray(array, SIZE);
+  ShowArray(array, SIZE, ShowElement);
+  ShowArray(array, SIZE, ShowElementInBrackets);
+  ShowArray(array, SIZE, ShowElementInSquareBrackets);
 
   return 0;
+}
+
+void InitializeArray(int array[], int size){
+  for(int i = 0; i < size; i++){
+    array[i] = i;
+  }
+}
+
+void ShowArray(int array[], int size, void (*showElement)(int element)){
+  for(int i = 0; i < size; i++){
+    showElement(array[i]);
+  }
+  std::cout << std::endl;
+}
+
+void ShowElement(int element){
+    std::cout << element << " ";
+}
+
+void ShowElementInBrackets(int element){
+  std::cout << "(" << element << ") ";
+}
+
+void ShowElementInSquareBrackets(int element){
+  std::cout << "[" << element << "] ";
 }
