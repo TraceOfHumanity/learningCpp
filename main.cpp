42b59fcd61e595d26105db3eb6eb8345c43d7c59
Alexander_Ovchar <azalexxx02@gmail.com>
2024-08-27 20:26:16 +0300

    main

--- main.cpp
+++ main.cpp
@@ -1,33 +1,7 @@
 #include <iostream>
 
-enum class Direction
-{
-  Up,
-  Down,
-  Left,
-  Right
-};
-
 int main()
 {
-
-  Direction heroDirection = Direction::Up;
-
-  switch (heroDirection)
-  {
-  case Direction::Up:
-    std::cout << "Up" << std::endl;
-    break;
-  case Direction::Down:
-    std::cout << "Down" << std::endl;
-    break;
-  case Direction::Left:
-    std::cout << "Left" << std::endl;
-    break;
-  case Direction::Right:
-    std::cout << "Right" << std::endl;
-    break;
-  }
   
   return 0;
 }
