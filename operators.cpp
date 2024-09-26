f1c6ccb76de6604f63fc77c0762f79e57ad0a178
Alexander_Ovchar <azalexxx02@gmail.com>
2024-09-26 20:09:02 +0300

    .

--- operators.cpp
+++ operators.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#define SHOW_STRING(str) std::cout << "Hello " #str << std::endl;
 #define CONCAT(a, b) a##b
+#define TO_STRING(a) #a
 
 int main()
 {
-  SHOW_STRING(123)
   std::cout << CONCAT(1, 2) << std::endl;
+  std::cout << TO_STRING(123) << std::endl;
   return 0;
 }
