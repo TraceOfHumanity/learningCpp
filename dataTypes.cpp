bda3810aa0a7018379cf3a614c328889638e0334
Alexander_Ovchar <azalexxx02@gmail.com>
2024-09-26 20:42:59 +0300

    Integer

--- dataTypes.cpp
+++ dataTypes.cpp
@@ -7,6 +7,20 @@ int main()
   std::cout << "WChar_t: " << sizeof(wchar_t) << " bytes\n";
   std::cout << "Char16_t: " << sizeof(char16_t) << " bytes\n";
   std::cout << "Char32_t: " << sizeof(char32_t) << " bytes\n";
-  
+
+  std::cout << std::endl;
+
+  std::cout << "------Integer------\n";
+  std::cout << "Short: " << sizeof(short) << " bytes\n";
+  std::cout << "Unsigned Short: " << sizeof(unsigned short) << " bytes\n";
+  std::cout << "Int: " << sizeof(int) << " bytes\n";
+  std::cout << "Unsigned Int: " << sizeof(unsigned int) << " bytes\n";
+  std::cout << "Long: " << sizeof(long) << " bytes\n";
+  std::cout << "Unsigned Long: " << sizeof(unsigned long) << " bytes\n";
+  std::cout << "Long Long: " << sizeof(long long) << " bytes\n";
+  std::cout << "Unsigned Long Long: " << sizeof(unsigned long long) << " bytes\n";
+
+  std::cout << std::endl;
+
   return 0;
 }
