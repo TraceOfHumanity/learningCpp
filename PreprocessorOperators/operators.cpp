dbaeac4890bb93f3ae44c5be53f725bd3ade4309
Alexander_Ovchar <azalexxx02@gmail.com>
2024-09-26 20:04:34 +0300

    CONCAT

--- PreprocessorOperators/operators.cpp
+++ PreprocessorOperators/operators.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #define SHOW_STRING(str) std::cout << "Hello " #str << std::endl;
-
-// using namespace std;
+#define CONCAT(a, b) a##b
 
 int main()
 {
   SHOW_STRING(123)
+  std::cout << CONCAT(1, 2) << std::endl;
   return 0;
 }
